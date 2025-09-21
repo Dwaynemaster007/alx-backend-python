bb7af4951ac2d799f159edf2f95b8065dc578d01
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-09-21 18:33:42 +0200

    feat: Defined serializers for User, Conversation, and Message models

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role']
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.ReadOnlyField(source='sender.email')
+    
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    messages = MessageSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
