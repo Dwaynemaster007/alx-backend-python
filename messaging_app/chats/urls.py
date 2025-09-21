0523defa14a7bd2b9990ec6555d7d731e0ceaa87
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-09-21 18:46:36 +0200

    feat: Implemented API ViewSets for User, Conversation, and Message

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,11 @@
 # chats/urls.py
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework import routers
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = router.urls
